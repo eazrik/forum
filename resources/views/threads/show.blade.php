535c923bdeb90aaabf646b842bf276ff11f59967
vagrant <vagrant@homestead>
2018-02-17 03:30:27 +0000

    initial file

---
+++ resources/views/threads/show.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card card-default">
+                <div class="card-header">{{ $thread->title }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    {{ $thread->body }}
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            @foreach ($thread->replies as $reply)
+            <br>
+                <div class="card card-default">
+                    <div class="card-header">
+                        <a href ="#">{{ $reply->owner->name }}</a> said {{ $reply->created_at->diffForHumans() }}...
+                    </div>
+                    <div class="card-body">
+                        {{ $reply->body }}
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+
+</div>
+@endsection
